362919a31cc9ffad48a0b694a75fc06508e2f305
navee <navee@NAVEEN>
2023-09-09 23:57:48 +0530

    second

--- src/main/java/com/springbatch/SpringBatchTaskletApplication.java
+++ src/main/java/com/springbatch/SpringBatchTaskletApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBatchTaskletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchTaskletApplication.class, args);
 	}
